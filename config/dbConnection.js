a085b3db8ded9a7774a35ceb59c99c6d361bc2f8
Lakshmi-kande <kandelakshmi500@gmail.com>
2023-03-23 19:06:20 +0530

    init

---
+++ config/dbConnection.js
@@ -0,0 +1,25 @@
+const mongoose= require ("mongoose");
+const connectDb = async()=>{
+    try{
+        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+        console.log(
+            "Database connected:", 
+            connect.connection.host,
+            connect.connection.name,
+        );
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDb;
+
+
+
+// const mongoose = require('mongoose');
+// const uri = 'mongodb://localhost:27017/mydatabase';
+
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('Connected to database'))
+//   .catch(error => console.error(error));
